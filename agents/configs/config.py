ec317bdea56eda8c6f7388a8fd5acb066c3d0559
johnnas12 <yohanesmesfin3@gmail.com>
2025-06-28 21:52:53 +0300

    chore: add metadata and embeddings of workflows

--- agents/configs/config.py
+++ agents/configs/config.py
@@ -4,6 +4,8 @@ class Settings(BaseSettings):
     embeddings_path: str = "embeddings/galaxy_embeddings.npy"
     metadata_path: str = "embeddings/galaxy_metadata.json"
     data_to_encode_path: str ="data/galaxy_tools_may_3_2025_v1.json"
+    workflow_embeddings_path: str = "agents/embeddings/iwc_workflow_embeddings.npy"
+    workflow_metadata_path: str = "agents/embeddings/iwc_workflow_metadata.json"
     allowed_origins: str = "*"
 
 settings = Settings()
