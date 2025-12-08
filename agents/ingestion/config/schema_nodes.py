d0dd3fcbdcc099d81ebf9dad172d8f7578668fc9
johnnas12 <yohanesmesfin3@gmail.com>
2025-12-08 14:07:56 +0300

    Adds configurable node and relationship schemas for ingestion
    Introduces YAML and Pydantic-based schema definitions for nodes and relationships in the ingestion pipeline, enabling flexible, strongly-typed modeling of workflows, tools, categories, steps, and their interconnections. Facilitates easier schema evolution and validation for ingesting graph-structured data.

---
+++ agents/ingestion/config/schema_nodes.py
@@ -0,0 +1,76 @@
+from pydantic import BaseModel #type: ignore
+from typing import Any
+
+class WorkflowProperties(BaseModel):
+    name: str
+    category: str
+    readme: str
+    workflow_repository: str
+    file_name: str
+    raw_download_url: str
+    number_of_steps: int
+    has_readme: bool
+    has_changelog: bool
+    has_test_data: bool
+
+class Workflow(BaseModel):
+    label: str = "workflow"
+    unique_key: Any = ['workflow_repository']
+    description: str = "A workflow composed of tools and IO"
+    properties: WorkflowProperties
+
+class ToolProperties(BaseModel):
+    name: str
+    description: str
+    version: str
+    help: str
+
+class Tool(BaseModel):
+    label: str = "tool"
+    unique_key: Any = ['name', 'version']
+    properties: ToolProperties
+
+class CategoryProperties(BaseModel):
+    name: str
+
+class Category(BaseModel):
+    label: str = "category"
+    unique_key: Any = ['name']
+    properties: CategoryProperties
+
+class InputProperties(BaseModel):
+    description: str
+
+class Input(BaseModel):
+    label: str = "input"
+    unique_key: Any = []
+    properties: InputProperties
+
+class OutputProperties(BaseModel):
+    description: str
+
+class Output(BaseModel):
+    label: str = "output"
+    unique_key: Any = []
+    properties: OutputProperties
+
+class KeywordProperties(BaseModel):
+    name: str
+
+class Keyword(BaseModel):
+    label: str = "keyword"
+    unique_key: Any = []
+    properties: KeywordProperties
+
+class StepProperties(BaseModel):
+    step_uid: str
+    step_id: int
+    name: str
+    type: str
+    annotation: str
+
+class Step(BaseModel):
+    label: str = "step"
+    unique_key: Any = ['step_uid']
+    description: str = "A single step in a workflow"
+    properties: StepProperties
