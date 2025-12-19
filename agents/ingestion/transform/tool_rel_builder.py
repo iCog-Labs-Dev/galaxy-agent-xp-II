bd4d90bf0da0342fc604562650873b8f5ee7f9ab
Henok Yoseph <henokapril@gmail.com>
2025-12-19 22:32:27 +0300

    tool relation builder

--- agents/ingestion/transform/tool_rel_builder.py
+++ agents/ingestion/transform/tool_rel_builder.py
@@ -1,15 +1,15 @@
 class ToolMetadataRelations:
     def tool_category(self, tool_node: dict, cat_node: dict):
         return (
-            "BELONGS_TO",
+            "BELONGS_TO",        
             "Tool", tool_node["properties"],
             "ToolCategory", cat_node["properties"],
-            {}
+            {}                     
         )
 
     def tool_has_input(self, tool_node: dict, input_node: dict):
         return (
-            "TOOL_HAS_INPUT",
+            "HAS_INPUT",           
             "Tool", tool_node["properties"],
             "ToolInput", input_node["properties"],
             {}
@@ -17,7 +17,7 @@ class ToolMetadataRelations:
 
     def tool_has_output(self, tool_node: dict, output_node: dict):
         return (
-            "TOOL_HAS_OUTPUT",
+            "HAS_OUTPUT",          
             "Tool", tool_node["properties"],
             "ToolOutput", output_node["properties"],
             {}
